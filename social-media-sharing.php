76562b922ec0f3714bf2f495c562a40685e7fa15
Jesse Overright <jesseoverright@gmail.com>
2013-10-24 09:10:32 -0500

    Refactored plugin into class structure

--- social-media-sharing.php
+++ social-media-sharing.php
@@ -3,7 +3,7 @@
  * Plugin Name: Social Media Sharing
  * Plugin URI: http://about.me/joverright
  * Description: Adds Facebook OpenGraph and Twitter Card support to your website.
- * Version: 0.1 beta
+ * Version: 0.5 beta
  * Author: Jesse Overright
  * Author URI: http://about.me/joverright
  * License: GPL2
@@ -25,77 +25,102 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define('TWITTER_HANDLE', '@YOUR_TWITTER_HANDLE');
-define('DEFAULT_SOCIAL_MEDIA_DESCRIPTION', 'DEFAULT DESCRIPTION');
-define('DEFAULT_SOCIAL_MEDIA_IMAGE', get_bloginfo('stylesheet_directory').'/images/DEFAULT_IMAGE.jpg' );
+if ( !class_exists('Social_Media_Sharing') ) :
 
-add_image_size( 'facebook-thumbnail', 300, 300);
+class Social_Media_Sharing {
+    private static $instance;
 
-add_action( 'wp_head', 'social_media_sharing_init');
-
-function social_media_sharing_init() {
-    global $post;
-
-    if ( is_single() || is_page()) {
-        if ( has_post_thumbnail($post->ID) ) { 
-            $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID,'facebook-thumbnail') );
-            $og_image = $thumbnail[0];
-        } else {
-            $og_image = DEFAULT_SOCIAL_MEDIA_IMAGE;
+    # enforce singleton design pattern
+    public static function get_instance() {
+        if ( !isset( self::$instance ) ) {
+            $class = __CLASS__;
+            self::$instance = new $class();
         }
 
-        $settings =  array (
-            'url'   => get_permalink(),
-            'title' => wp_title( '|', false, 'right'),
-            'image' => $og_image,
-        );
-    } else {
-        $settings = array (
-            'url'   => $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
-            'title' => wp_title( '|', false, 'right'),
-            'image' => DEFAULT_SOCIAL_MEDIA_IMAGE,
-        );
+        return self::$instance;
     }
 
-    get_facebook_opengraph($settings);
-    get_twitter_card_meta($settings);
-}
+    protected function __construct() {
+        # define defaults
+        define('TWITTER_HANDLE', '@YOUR_TWITTER_HANDLE');
+        define('DEFAULT_SOCIAL_MEDIA_DESCRIPTION', 'DEFAULT DESCRIPTION');
+        define('DEFAULT_SOCIAL_MEDIA_IMAGE', get_bloginfo('stylesheet_directory').'/images/DEFAULT_IMAGE.jpg' );
 
-function get_facebook_opengraph($settings) { ?>
-    <meta property="og:image" content="<?= $settings['image'] ?>" />
-    <meta property="og:url" content="<?= $settings['url'] ?>" />
-    <meta property="og:site_name" content="<?= site_url() ?>" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="<?= $settings['title'] ?>" />
-    <meta property="og:description" content="<?= get_socialmedia_excerpt(35) ?>" />
-    <?php
-}
+        # set up facebook thumbnail size
+        add_image_size( 'facebook-thumbnail', 300, 300);
 
-function get_twitter_card_meta($settings) { ?>
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="<?= TWITTER_HANDLE ?>" />
-    <meta name="twitter:creator" content="<?= TWITTER_HANDLE ?>" />
-    <meta name="twitter:url" content="<?= $settings['url'] ?>" />
-    <meta name="twitter:title" content="<?= $settings['title'] ?>" />
-    <meta name="twitter:description" content="<?= get_socialmedia_excerpt(25) ?>" />
-    <?php
-}
+        # initialize the social media sharing display
+        add_action( 'wp_head', array( $this, 'social_media_sharing_init') );
+    }
 
-function get_socialmedia_excerpt($excerpt_length = '20'){
-    if ( is_single() || is_page() ) {
+    public function social_media_sharing_init() {
         global $post;
-        $the_excerpt = $post->post_content; //Gets post_content to be used as a basis for the excerpt
-        $the_excerpt = strip_tags(strip_shortcodes($the_excerpt)); //Strips tags and images
-        $words = explode(' ', $the_excerpt, $excerpt_length + 1);
-        if(count($words) > $excerpt_length) :
-            array_pop($words);
-            array_push($words, '…');
-            $the_excerpt = implode(' ', $words);
-        endif;
-        // remove any quotes
-        $the_excerpt = str_replace('"','',$the_excerpt);
+
+        if ( is_single() || is_page()) {
+            if ( has_post_thumbnail($post->ID) ) { 
+                $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID,'facebook-thumbnail') );
+                $og_image = $thumbnail[0];
+            } else {
+                $og_image = DEFAULT_SOCIAL_MEDIA_IMAGE;
+            }
+
+            $settings =  array (
+                'url'   => get_permalink(),
+                'title' => wp_title( '|', false, 'right'),
+                'image' => $og_image,
+            );
+        } else {
+            $settings = array (
+                'url'   => $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
+                'title' => wp_title( '|', false, 'right'),
+                'image' => DEFAULT_SOCIAL_MEDIA_IMAGE,
+            );
+        }
+
+        $this->get_facebook_opengraph($settings);
+        $this->get_twitter_card_meta($settings);
     }
-    
-    if ($the_excerpt == '') $the_excerpt = DEFAULT_SOCIAL_MEDIA_DESCRIPTION; 
-    return $the_excerpt;
+
+    protected function get_facebook_opengraph($settings) { ?>
+        <meta property="og:image" content="<?= $settings['image'] ?>" />
+        <meta property="og:url" content="<?= $settings['url'] ?>" />
+        <meta property="og:site_name" content="<?= site_url() ?>" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="<?= $settings['title'] ?>" />
+        <meta property="og:description" content="<?= $this->get_socialmedia_excerpt(35) ?>" />
+        <?php
+    }
+
+    protected function get_twitter_card_meta($settings) { ?>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="<?= TWITTER_HANDLE ?>" />
+        <meta name="twitter:creator" content="<?= TWITTER_HANDLE ?>" />
+        <meta name="twitter:url" content="<?= $settings['url'] ?>" />
+        <meta name="twitter:title" content="<?= $settings['title'] ?>" />
+        <meta name="twitter:description" content="<?= $this->get_socialmedia_excerpt(25) ?>" />
+        <?php
+    }
+
+    protected function get_socialmedia_excerpt($excerpt_length = '20'){
+        if ( is_single() || is_page() ) {
+            global $post;
+            $the_excerpt = $post->post_content; //Gets post_content to be used as a basis for the excerpt
+            $the_excerpt = strip_tags(strip_shortcodes($the_excerpt)); //Strips tags and images
+            $words = explode(' ', $the_excerpt, $excerpt_length + 1);
+            if(count($words) > $excerpt_length) :
+                array_pop($words);
+                array_push($words, '…');
+                $the_excerpt = implode(' ', $words);
+            endif;
+            // remove any quotes
+            $the_excerpt = str_replace('"','',$the_excerpt);
+        }
+        
+        if ($the_excerpt == '') $the_excerpt = DEFAULT_SOCIAL_MEDIA_DESCRIPTION; 
+        return $the_excerpt;
+    }
 }
+
+Social_Media_Sharing::get_instance();
+
+endif;
